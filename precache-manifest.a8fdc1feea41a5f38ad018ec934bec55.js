7348fc84f9c14f492bb011cd0f13c8e88a5fcfcd
kabir7830 <kabir.behal7830@gmail.com>
2022-11-24 23:27:58 +0530

    Updates

---
+++ precache-manifest.a8fdc1feea41a5f38ad018ec934bec55.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d632fc2448f10a08ce9",
+    "url": "/Kabir7830/LMS.git/static/css/main.3f34ae35.chunk.css"
+  },
+  {
+    "revision": "2d632fc2448f10a08ce9",
+    "url": "/Kabir7830/LMS.git/static/js/main.2d632fc2.chunk.js"
+  },
+  {
+    "revision": "68d85c9b0ba336868cc8",
+    "url": "/Kabir7830/LMS.git/static/js/1.68d85c9b.chunk.js"
+  },
+  {
+    "revision": "0a7323a627c3f23f0a13",
+    "url": "/Kabir7830/LMS.git/static/js/2.0a7323a6.chunk.js"
+  },
+  {
+    "revision": "117eea022a4ba766ded0",
+    "url": "/Kabir7830/LMS.git/static/js/runtime~main.117eea02.js"
+  },
+  {
+    "revision": "f79c8bc1756ff0e03068f1673001b0b7",
+    "url": "/Kabir7830/LMS.git/static/media/lms_logo3.f79c8bc1.png"
+  },
+  {
+    "revision": "25c311e1d190a0097fe3e4fbeafcc7f8",
+    "url": "/Kabir7830/LMS.git/static/media/settingsLogo.25c311e1.png"
+  },
+  {
+    "revision": "8cbcd6bd631e68f3dba749cf468d8009",
+    "url": "/Kabir7830/LMS.git/static/media/gmail5.8cbcd6bd.png"
+  },
+  {
+    "revision": "2bc16f18aeb18e8f4161589130834871",
+    "url": "/Kabir7830/LMS.git/static/media/twitter1.2bc16f18.png"
+  },
+  {
+    "revision": "a972ebab748f23d8a8f141a84f07d5f2",
+    "url": "/Kabir7830/LMS.git/index.html"
+  }
+];
